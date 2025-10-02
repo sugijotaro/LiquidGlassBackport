1e4e0666636115289a01b98bba318ccd6acc7a3f
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2025-10-02 15:51:53 +0900

    Update ContentView.swift

--- sample/LiquidGlassBackportDemoApp/LiquidGlassBackportDemoApp/ContentView.swift
+++ sample/LiquidGlassBackportDemoApp/LiquidGlassBackportDemoApp/ContentView.swift
@@ -47,6 +47,7 @@ struct ContentView: View {
                     .glassEffectIfAvailable()
             }
             .padding()
+            .frame(maxWidth: .infinity)
         }
         .background {
             Image("Wallpaper")

d0237eba599be24b99482dfb86a0fe6cc01e1dfa
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2025-10-02 15:37:31 +0900

    demoapp

---
+++ sample/LiquidGlassBackportDemoApp/LiquidGlassBackportDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiquidGlassBackportDemoApp
+//
+//  Created by Jotaro Sugiyama on 2025/10/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sample/LiquidGlassBackportDemoApp/LiquidGlassBackportDemoApp/LiquidGlassBackportDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlassBackportDemoAppApp.swift
+//  LiquidGlassBackportDemoApp
+//
+//  Created by Jotaro Sugiyama on 2025/10/02.
+//
+
+import SwiftUI
+
+@main
+struct LiquidGlassBackportDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
